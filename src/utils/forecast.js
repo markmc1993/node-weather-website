b00b336d1a61bdfab160b8ffa839286852d096e0
Mark McAllister <37349839+markmc1993@users.noreply.github.com>
2022-10-23 16:39:28 +0100

    Added more weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,8 @@ const forecast = (lat, long, callback) => {
 
         //if no error and a proper location was provided callback string
         } else{
-            callback(undefined, (body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degreees out. It feels like ' + body.current.feelslike  + ' degrees out.'))
+            callback(undefined, (body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degreees out. It feels like ' + body.current.feelslike  + ' degrees out.\
+            The humidity is ' + body.current.humidity + ' and cloud cover is ' + body.current.cloudcover))
         }
     })
 }
